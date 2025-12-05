fc016f63808afa735611fb013497005dbf4f5cb5
Artem Onyshchenko <artemon888.com@gmail.com>
2025-12-05 10:38:22 +0200

    Add peerjs.min.js and fix download-libs.bat script

--- app/android/download-libs.bat
+++ app/android/download-libs.bat
@@ -4,25 +4,30 @@ echo Downloading libraries for Android
 echo ========================================
 echo.
 
-cd app\src\main\assets
-
 echo Downloading jQuery...
-curl -o jquery.min.js https://code.jquery.com/jquery-3.6.0.min.js
+powershell -Command "Invoke-WebRequest -Uri 'https://code.jquery.com/jquery-3.6.0.min.js' -OutFile 'app\src\main\assets\jquery.min.js'"
 
 echo Downloading Chess.js...
-curl -o chess.js https://cdnjs.cloudflare.com/ajax/libs/chess.js/0.10.3/chess.min.js
+powershell -Command "Invoke-WebRequest -Uri 'https://cdnjs.cloudflare.com/ajax/libs/chess.js/0.10.3/chess.min.js' -OutFile 'app\src\main\assets\chess.js'"
 
 echo Downloading Chessboard.js CSS...
-curl -o chessboard.min.css https://unpkg.com/@chrisoakman/chessboardjs@1.0.0/dist/chessboard-1.0.0.min.css
+powershell -Command "Invoke-WebRequest -Uri 'https://unpkg.com/@chrisoakman/chessboardjs@1.0.0/dist/chessboard-1.0.0.min.css' -OutFile 'app\src\main\assets\chessboard.min.css'"
 
 echo Downloading Chessboard.js...
-curl -o chessboard.min.js https://unpkg.com/@chrisoakman/chessboardjs@1.0.0/dist/chessboard-1.0.0.min.js
+powershell -Command "Invoke-WebRequest -Uri 'https://unpkg.com/@chrisoakman/chessboardjs@1.0.0/dist/chessboard-1.0.0.min.js' -OutFile 'app\src\main\assets\chessboard.min.js'"
+
+echo Downloading PeerJS...
+powershell -Command "Invoke-WebRequest -Uri 'https://unpkg.com/peerjs@1.5.2/dist/peerjs.min.js' -OutFile 'app\src\main\assets\peerjs.min.js'"
 
 echo Downloading Stockfish.js...
-curl -o stockfish.js https://cdnjs.cloudflare.com/ajax/libs/stockfish.js/10.0.2/stockfish.js
+powershell -Command "Invoke-WebRequest -Uri 'https://cdnjs.cloudflare.com/ajax/libs/stockfish.js/10.0.2/stockfish.js' -OutFile 'app\src\main\assets\stockfish.js'"
 
 echo.
 echo ========================================
-echo Done! Libraries downloaded
+echo Done! All libraries downloaded
 echo ========================================
+echo.
+echo Files in assets:
+dir app\src\main\assets\*.js
+echo.
 pause
